f24d46ab816e0adcc143e9ddc67ebdbdbe4d6308
Javier Chacon <javierchacon262@gmail.com>
2020-11-14 17:06:35 -0500

    Metodo de evaluaciion terminado

--- level1.m
+++ level1.m
@@ -1,20 +1,34 @@
 function costo = level1(matds, dems, cveh1, cost_t)
 %LEVEL1 Summary of this function goes here
-ss               = size(dems, 1);
-costo.ruta       = [0];
-costo.cost       = [];
-i                = 1;
-vis              = [];
+
+ss                      = size(dems, 1);
+costo.ruta              = [0];
+costo.cost              = [];
+i                       = 1;
+vis                     = [];
+cap                     = cveh1;
+vehs                    = 1;
 while i <= ss
-    [dist, next] = cercano(matds, vis, costo.ruta(i)+1);
-    costo.ruta   = [costo.ruta, next];
-    costo.cost   = [costo.cost, dist*cost_t];
-    i            = i + 1;
-    vis          = [vis, costo.ruta(end)+1];
+    if cap >= dems(i)
+        [dist, next]    = cercano(matds, vis, costo.ruta(end)+1);
+        costo.ruta      = [costo.ruta, next];
+        costo.cost      = [costo.cost, dist*cost_t];        
+        vis             = [vis, costo.ruta(end)+1];
+        cap             = cap - dems(i);
+        i               = i + 1;        
+    else
+        cap             = cveh1;
+        vehs            = vehs + 1;
+        costo.ruta      = [costo.ruta, 0];
+        costo.cost(i-1) = costo.cost(i-1) + (matds(i-1, 1)*cost_t);
+    end
 end
-idx              = vis(end);
-dist             = matds(idx, 1);
-costo.cost       = [costo.cost, dist*cost_t];
-costo.ruta       = [costo.ruta, 0];
+
+idx                     = costo.ruta(end);
+dist                    = matds(idx+1, 1);
+costo.cost              = [costo.cost, dist*cost_t];
+costo.ruta              = [costo.ruta, 0];
+costo.vehs              = vehs;
+costo.dems              = dems;
 end
 
